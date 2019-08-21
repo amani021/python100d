f79139b6d495ce505f9c3f1435b7f3e4dc69ce53
amani021 <003a48@gmail.com.com>
2019-08-21 13:02:02 +0300

    2nd lesson

--- day_1.py
+++
@@ -1,5 +0,0 @@
-print("Hello, World!")
-exit()
-
-if 5 > 2:
-    print("Five is greater than two!")
---
+++ week_1.py
@@ -0,0 +1,23 @@
+#-------- DAY 1 "print" --------
+# print("Hello, World!")
+# #exit()
+#
+# if 5 > 2:
+#     print("Five is greater than two!")
+
+
+#-------- DAY 2 "comments" --------
+'''
+Comments have 2 ways to write:
+1- One line, starts with #
+2- Multiline, by place a text between """  """ or '''  '''
+'''
+#How to print in python
+print("Good Evening Guys")
+print("How do you do?")
+print("Are you ready to study after few days?!")
+#exit() #Exit from cmd (after this line, it will ignore)
+
+#The identation is very important in python
+# if 9 < 10:
+#     print("Nine is less than ten!")
