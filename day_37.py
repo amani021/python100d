b25df6541dc8d74c10b379334ca1affec5c6c557
amani021 <003a48@gmail.com.com>
2019-09-25 18:10:03 +0300

    37th lesson

---
+++ day_37.py
@@ -0,0 +1,10 @@
+#-------- DAY 37 "arrays1" --------
+# 1. Create an array
+# 2. Assign to the array
+# 3. Access the array items by referring to the index number
+# 4. Change an item value
+# 5. Use len() method to count the number of length, as we used with strings, lists, tuples, sets & dictionaries
+arr = ['list', 'tuple', 'set', 'dictionary', 'array']
+text = "Lesson {}: {} 1"
+arr[4] = 'Arrays'
+print(text.format(len(arr)*7+2, arr[4]))
