38f9704d4d33f877c467f9c51efe717973543439
amani021 <003a48@gmail.com.com>
2019-10-08 23:22:20 +0300

    50th lesson

---
+++ Module.py
@@ -0,0 +1,12 @@
+#-------- MODULE --------
+# You can write in modules anythings you want
+# As we learned before (variables, functions, lists, sets...)
+# Then, in any file of project, you can access to a module writing import at beginning
+#Variables in module
+colors = {
+1: 'red',
+2: 'white',
+3: 'blue'
+}
+def txt(name):
+    print('Hello, my name is', name)
---
+++ day_50.py
@@ -0,0 +1,6 @@
+#-------- DAY 50 "modules1" --------
+import Module #Using a module file
+
+print("Lesson 50: Modules 1")
+Module.txt('Amani') #Access to its function
+print("I like", Module.colors[1], "color")
