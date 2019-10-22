2ebc0cede76df9ef9022cfe990d8fa9e205d3476
amani021 <003a48@gmail.com.com>
2019-10-22 23:42:03 +0300

    64th lesson

---
+++ day_64.py
@@ -0,0 +1,27 @@
+#-------- DAY 64 "try-except" --------
+print("Lesson 64: Try-Except")
+try:
+    print(name) #Will rais an error!!
+except:
+    print('An exception occurred!!')
+
+# try:
+#     print(name) #Will rais an error!!
+# except NameError:
+#     print('Variable name is not defined')
+# except:
+#     print('Something else went wrong')
+
+# try:
+#     print('Hello World')
+# except:
+#     print('Something went wrong')
+# else:
+#     print('Nothing went wrong')
+
+# try:
+#     print('Hello World')
+# except:
+#     print('Something went wrong')
+# finally:
+#     print("The 'try except' is finished")
