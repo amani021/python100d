c500d2066b687310579df1bcbec90cf162158053
amani021 <003a48@gmail.com.com>
2019-10-26 16:54:57 +0300

    68th lesson

---
+++ day_68.py
@@ -0,0 +1,7 @@
+#-------- DAY 68 "extra exercise" --------
+arr = []
+size = int(input("How much the elements do you want in an array? "))
+print("Enter the elements:")
+for x in range(size):
+    arr.append(input())
+print("\nNow, the array with your elements is:\n", arr)
